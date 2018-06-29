f66abf727597ea0f35ebf656b1039e5726252e67
Celso Jr <celso.cllsj@gmail.com>
2018-06-28 23:18:23 -0300

    Commit 3
    Assets

---
+++ Assets/TrafficControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class TrafficControl : MonoBehaviour {
+
+	bool raceStarted = false;
+	GameObject[] AICars;
+	GameObject sinaleira;
+	GameObject paradaCarro;
+	float topspeed;
+
+	// Use this for initialization
+	void Start () {
+		AICars = GameObject.FindGameObjectsWithTag("AICar");
+		sinaleira = GameObject.FindGameObjectWithTag("Sinaleira");
+		//paradaCarro = GameObject.FindGameObjectWithTag("paradaCarro");
+
+		foreach (GameObject car in AICars) 
+		{
+			topspeed = car.GetComponent<CarController>().GetVelocidadeMaximaCarro();
+		}
+
+		/*foreach (GameObject car in AICars) 
+		{
+			car.GetComponent<CarAIControl>().enabled = false;
+		}
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sinaleira.GetComponent<TimerSinaleira>().GetSinalAmareloStatus() == true) 
+		{
+			foreach (GameObject car in AICars) 
+			{
+				car.GetComponent<CarController>().SetarTopSpeed(1);
+			}
+		}
+		else if (sinaleira.GetComponent<TimerSinaleira>().GetSinalVermelhoStatus() == true) 
+		{
+			foreach (GameObject car in AICars) 
+			{
+				car.GetComponent<CarController>().SetarTopSpeed(0);
+			}
+		}
+		else{
+			foreach (GameObject car in AICars) 
+			{
+				car.GetComponent<CarController>().SetarTopSpeed(topspeed);
+			}
+		}
+		
+	}
+}
